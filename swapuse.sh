039500aae4a4dee41e3957bca6940964e6cbdf3c
Tierra Rounder <105559398+tierrarounder@users.noreply.github.com>
2022-05-29 14:21:19 -0500

    added echo lines as a test

--- swapuse.sh
+++ swapuse.sh
@@ -1,3 +1,8 @@
 # bash script for manipulating swapfile settings
 #
 #!/bin/bash
+echo '		RAM				SWAP	'
+echo '		<=1Gb			2Gb		'
+echo '		2-4Gb			2Gb		'
+echo '		 8Gb			4Gb		'
+echo '		>8Gb			2-4Gb	'
